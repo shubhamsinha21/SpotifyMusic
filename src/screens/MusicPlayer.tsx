4432b26b1c4deed2a86dbbc26ce99985f554f01f
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-22 14:05:34 +0530

    MusicPlayer.tsx done

---
+++ src/screens/MusicPlayer.tsx
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+import {View, StyleSheet, Dimensions, Image, FlatList} from 'react-native'
+import TrackPlayer, {Event, Track, useTrackPlayerEvents} from 'react-native-track-player'
+import { playListData } from '../constants'
+import SongInfo from '../components/SongsInfo'
+import SongSlider from '../components/SongSlider'
+import ControlCentre from '../components/ControlCenter'
+
+
+const {width} = Dimensions.get('window')
+
+const MusicPlayer = () => {
+
+    const [track, setTrack] = useState<Track | null>()
+
+    useTrackPlayerEvents([Event.PlaybackTrackChanged], async event => {
+            switch (event.type) {
+                case Event.PlaybackTrackChanged:
+                    const playingTrack = await TrackPlayer.getTrack(event.nextTrack)
+                    setTrack(playingTrack)
+                    break;
+            }
+    })
+
+    const renderArtWork = () => (
+        <View style={styles.listArtWrapper}>
+            <View style={styles.albumContainer}>
+                {track?.artwork && (
+                    <Image
+                    style={styles.albumArtImage}
+                    source={{uri:track?.artwork?.toString()}}
+                    />
+                )}
+            </View>
+        </View>
+    )
+
+    // Main View of the App
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    horizontal
+                    data={playListData}
+                    renderItem={renderArtWork}
+                    keyExtractor={song => song.id.toString()}
+                />
+
+            <SongInfo track={track}/>
+            <SongSlider/>
+            <ControlCentre/>
+
+            </View>
+        )
+}
+
+const styles = StyleSheet.create({
+    listArtWrapper:{
+
+    },
+    albumContainer:{
+
+    },
+    albumArtImage:{
+
+    },
+    container:{
+
+    },
+
+
+})
+
+export default MusicPlayer;
